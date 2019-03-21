e055d83e98b8b67153a59200a965d2bf7a19d9b3
eggyo <eggyoyo4859@gmail.com>
2019-03-22 00:33:04 +0700

    v.1

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://arty-106-inventory.herokuapp.com/';
+$config['base_url'] = 'https://arty-106-ammo.herokuapp.com/';
 //$config['base_url'] = 'http://localhost/arty106-inventory/';
 
 /*
